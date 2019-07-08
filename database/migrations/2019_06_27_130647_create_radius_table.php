c8053e44a10a4106634e746975908d8c0a2371cd
zhongkon <48372790+zhongkon@users.noreply.github.com>
2019-07-08 16:22:19 +0700

    Update 2019_06_27_130647_create_radius_table.php

--- database/migrations/2019_06_27_130647_create_radius_table.php
+++ database/migrations/2019_06_27_130647_create_radius_table.php
@@ -13,7 +13,7 @@ class CreateRadiusTable extends Migration
      */
     public function up()
     {
-        Schema::connection('mysql4')->create('radcheck', function (Blueprint $table) {
+        Schema::connection('mysql2')->create('radcheck', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('username',64)->default('');
             $table->string('attribute',64)->default('');
@@ -21,7 +21,7 @@ class CreateRadiusTable extends Migration
             $table->string('value',253)->default('');
         });
 
-        Schema::connection('mysql4')->create('radgroupreply', function (Blueprint $table) {
+        Schema::connection('mysql2')->create('radgroupreply', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('groupname',64)->default('');
             $table->string('attribute',64)->default('');
@@ -30,7 +30,7 @@ class CreateRadiusTable extends Migration
         });
 
 
-        Schema::connection('mysql4')->create('radreply', function (Blueprint $table) {
+        Schema::connection('mysql2')->create('radreply', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('username',64)->unique()->default('');
             $table->string('attribute',64)->default('');
@@ -38,14 +38,14 @@ class CreateRadiusTable extends Migration
             $table->string('value',253)->nullable();                
         });
 
-        Schema::connection('mysql4')->create('radusergroup', function (Blueprint $table) {
+        Schema::connection('mysql2')->create('radusergroup', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('username',64)->default('');
             $table->string('groupname',64)->default('');
             $table->integer('priority')->default('1');
         });
 
-        Schema::connection('mysql4')->create('radpostauth', function (Blueprint $table) {
+        Schema::connection('mysql2')->create('radpostauth', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('username',64)->default('');
             $table->string('pass',64)->default('');
